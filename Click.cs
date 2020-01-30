3e8c42d65e4ea5ba30afdeb796029d858310f884
wallp225 <54846650+wallp225@users.noreply.github.com>
2020-01-30 15:29:24 +0000

    Intial File Upload
    Includes Click, that handles pressing of buttons, and Interlock Check, that may or may not handle the logic to determine if a button can be pressed or not.

---
+++ Click.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    // Toggle a button between its pressed and unpressed state
+
+    public GameObject switch1;
+    public GameObject switch2;
+    public float moveValue = 0.15f; // value to move button by
+    public float upPos = 0.3f;      // default button position
+    Vector3 tempPos;
+    public bool switchPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch1 = GameObject.FindWithTag("Switch1");
+        switch2 = GameObject.FindWithTag("Switch2");
+
+        Debug.Log("thisSwitch = " + switch1.name);
+        Debug.Log("otherSwitch = " + switch2.name);
+
+        tempPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(tempPos.y < upPos)
+        {
+            Debug.Log("Button Up");
+            tempPos.y += moveValue;
+            switchPressed = false;
+        } else {
+            Debug.Log("Button Down");
+            tempPos.y += -moveValue;
+            switchPressed = true;
+        }
+        transform.position = tempPos;
+        Debug.Log("switch1 pressed? " + switch1.GetComponent<Click>().switchPressed);
+        Debug.Log("switch2 pressed? " + switch2.GetComponent<Click>().switchPressed);
+    }
+}
